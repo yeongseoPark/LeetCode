657f2eea0b979d25275a3de4f0e40b9a58314402
YSEO <1park4170@gmail.com>
2023-12-30 16:24:06 +0900

    Time: 942 ms (5.15%), Space: 29 MB (53.74%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,10 +1,3 @@
 class Solution(object):
     def containsDuplicate(self, nums):
-        saw = {}
-        for i in nums:
-            if i in saw and saw[i] >= 1:
-                return True 
-            
-            saw[i] = saw.get(i,0) + 1
-        
-        return False
+        return len(nums) != len(list(set(nums)))
