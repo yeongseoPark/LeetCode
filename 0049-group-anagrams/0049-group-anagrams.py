f0db1e9adc7566d8dd4b253c0a7bb002a83b446c
YSEO <1park4170@gmail.com>
2023-12-31 14:10:23 +0900

    Time: 84 ms (23.87%), Space: 17.7 MB (47.26%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -3,21 +3,13 @@ class Solution(object):
         if len(strs) == 1:
             return [strs]
         
-        sorted_map = {}
-        ans = []
+        sorted_map = defaultdict(list)
         
         for string in strs:
             sorted_string = str(sorted(string))
             
-            
-            if sorted_string in sorted_map:
                 # 무언가 답에 추가해주는 행위
-                sorted_map[sorted_string].append(string)
-            
-            else:
-                sorted_map[sorted_string] = [string]
+            sorted_map[sorted_string].append(string)
+
         
-        for key in sorted_map.keys():
-            ans.append(sorted_map[key])
-        
-        return ans
+        return list(sorted_map.values())
