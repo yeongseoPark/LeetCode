9df66fe0b21e82e6eab6d21219c1434de1e8e5f7
YSEO <1park4170@gmail.com>
2024-01-02 20:09:43 +0900

    Time: 403 ms (5.03%), Space: 20.3 MB (91.48%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -3,22 +3,15 @@ class Solution(object):
         
         n = len(nums)
         
-        left_product = [0] * n
-        right_product = [0] * n
-        product_no_self = [0] * n
-        
-        left_product[0] = 1
-        right_product[n-1] = 1
+        product_no_self = [1] * n
         
         for i in range(1, n):
-            left_product[i] = left_product[i-1] * nums[i-1]
+            product_no_self[i] = product_no_self[i-1] * nums[i-1]
         
-        for i in range(n-2, -1, -1):
-            right_product[i] = right_product[i+1] * nums[i+1]
-        
-        for i in range(n):
-            product_no_self[i] = right_product[i] * left_product[i]
-        
+        right = 1
+        for i in range(n-1, -1, -1):
+            product_no_self[i] *= right
+            right = right * nums[i]
+            
         return product_no_self
-        
                 
