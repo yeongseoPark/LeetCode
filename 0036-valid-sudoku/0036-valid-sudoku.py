534cccd273aad6958604e3f90ad4354ef282959c
YSEO <1park4170@gmail.com>
2024-01-06 12:46:06 +0900

    Time: 71 ms (26.36%), Space: 13.5 MB (12.10%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,36 +1,25 @@
 class Solution(object):
     def isValidSudoku(self, board):
-        def validate(row, col):
-            num = board[row][col]
-            
-            if num != ".":
-            
-                for c in range(9):
-                    if board[row][c] == num and c != col:
+        dupl_set = set()
+        
+        for i in range(9):
+            for j in range(9):
+                num = board[i][j]
+                if num != '.':
+                    if (str(num) + " in row " + str(i)) in dupl_set:
                         return False
-
-                for r in range(9):
-                    if board[r][col] == num and r != row:
+                    dupl_set.add(str(num) + " in row " + str(i))
+                    
+                    if (str(num) + " in col " + str(j)) in dupl_set:
                         return False
-
-                r = row // 3
-                c = col // 3
-
-                for i in range(r*3, r*3 + 3):
-                    for j in range(c*3, c*3 + 3):
-                        if board[i][j] == num and (i,j) != (row, col):
-                            return False
-
-            return True
-        
-        
-        for r in range(9):
-            for c in range(9):
-                if validate(r,c) == False:
-                    return False 
-        
-        return True 
-    
+                    
+                    dupl_set.add(str(num) + " in col " + str(j))
+                    
+                    if (str(num) + "in box " + str(i // 3) + str(j // 3)) in dupl_set:
+                        return False
+                    
+                    dupl_set.add(str(num) + "in box " + str(i // 3) + str(j // 3))
+        return True
         
 
                     
