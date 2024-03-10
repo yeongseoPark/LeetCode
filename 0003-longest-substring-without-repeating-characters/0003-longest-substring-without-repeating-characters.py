d17911c1c0ab442ec3e7b7bb9b12be745b01ca54
YSEO <1park4170@gmail.com>
2024-03-10 15:15:43 +0900

    Time: 23 ms (96.44%), Space: 12.4 MB (61.62%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -6,13 +6,13 @@ class Solution(object):
         lo = 0
 
         for hi in range(len(s)):
-            if s[hi] not in chars:
+            if s[hi] not in chars: 
                 chars.add(s[hi])
                 max_len = max(max_len, hi - lo + 1)
-            else:
-                while s[hi] in chars:
-                    chars.remove(s[lo])
-                    lo += 1
-                chars.add(s[hi])
+            else: # 중복 발생
+                while s[hi] in chars: # 중복되는게 substring 안에 없을때까지 
+                    chars.remove(s[lo]) # 중복을 제거하고
+                    lo += 1 # 시작점을 높임
+                chars.add(s[hi]) # 시작점이 다시 정해졌으니 여기서부터 다시 길이재기 시작
 
         return max_len
