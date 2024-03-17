1e4e06982cea58d7ef9d6ea6b7ea387aa6e1e8ce
YSEO <1park4170@gmail.com>
2024-03-17 16:41:18 +0900

    Time: 0 ms (100.00%), Space: 42 MB (19.17%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,25 +1,16 @@
-import java.math.BigInteger;
-
-
 class Solution {
     public int[] plusOne(int[] digits) {
-        StringBuilder s = new StringBuilder();
-
-        for (int i = 0; i < digits.length; i++) {
-            s.append(digits[i]);
+        int carry = 1;
+        for (int i = digits.length - 1; i >= 0; i--) {
+            digits[i] += carry;
+            if (digits[i] <= 9) {
+                return digits;
+            }
+            digits[i] = 0;
         }
 
-        BigInteger a = new BigInteger(s.toString());
-
-        a = a.add(BigInteger.ONE);
-
-        String string = a.toString();
-        int[] ints = new int[string.length()];
-        for (int i = 0; i < string.length(); i++)
-        {
-            ints[i] = Integer.valueOf(string.charAt(i) - '0');
-        }
-
-        return ints;
+        int[] ret = new int[digits.length + 1];
+        ret[0] = 1;
+        return ret;
     }
 }
